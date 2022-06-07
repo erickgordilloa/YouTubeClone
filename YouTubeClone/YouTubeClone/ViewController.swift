1e4d9d7f5ba47aa2c59a2dcc1d5f87bfb02bbe65
Erick Gordillo <egordilloayala@gmail.com>
2022-06-07 14:50:57 -0500

    init commit

---
+++ YouTubeClone/YouTubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by Erick Gordillo on 3/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
